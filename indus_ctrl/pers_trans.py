a661f849da51cbbda44418edac1534ed8ea591fa
mu9enn <sunx@4382.gmail.com>
2025-04-09 17:20:17 +0800

    complete dist_scale

--- indus_ctrl/pers_trans.py
+++ indus_ctrl/pers_trans.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 from camera import *
 
-
+def select_points(img, point_num=4, show_point=None):
     pts = []
     img_copy = img.copy()
 
@@ -16,21 +16,26 @@ from camera import *
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
             cv2.imshow(f"Select {point_num} Points", img_copy)
 
+    if show_point:
+        cv2.circle(img_copy, show_point, 5, (255, 0, 0), -1)
     cv2.imshow(f"Select {point_num} Points", img_copy)
     cv2.setMouseCallback(f"Select {point_num} Points", click_event)
+
     if point_num == 4:
+        print(f"请依次点击图像上的 {point_num} 个点（顺序：左上 -> 左下 -> 右下 -> 右上）")
     else:
+        print(f"请点击图像上的 {point_num} 个点。")
+
     while True:
         cv2.imshow(f"Select {point_num} Points", img_copy)
+        if cv2.waitKey(1) & 0xFF == 27:
             break
         if len(pts) == point_num:
             break
     cv2.destroyWindow(f"Select {point_num} Points")
     return pts
 
-
 def compute_trans(img, pts):
-    # 将选取的{point_num}个点转换为浮点型数组
     pts1 = np.float32(pts)
     h, w = img.shape[:2]
     pts2 = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]])
@@ -40,9 +45,8 @@ def compute_trans(img, pts):
 
     return matrix, result
 
-
 if __name__ == "__main__":
-    save_dir = r"D:\code_proj\uestc_projs\indus_ctrl"  # 图片保存路径
+    save_dir = r"D:\code_proj\uestc_projs\indus_ctrl"
     img = capture_image(save_dir, cam=1)
     pts = select_points(img)
     matrix, result = compute_trans(img, pts)
